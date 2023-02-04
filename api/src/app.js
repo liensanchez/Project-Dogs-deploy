220c86f9dc87ca60cfac14b9e1e2c199db2a4881
liensanchez <liensilviosanchez@gmail.com>
2023-02-04 00:44:33 -0300

    frontend & backend connection

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://project-dogs-deploy.vercel.app/home'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://deploy-ruby.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
